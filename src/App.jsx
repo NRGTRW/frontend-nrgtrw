b5dbf687ee5264438523475172a962435aecf0dd
NRGoranov <nrgoranov@gmail.com>
2025-01-07 18:09:57 +0200

    LogIn/SignUp

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,8 @@ import ProductPage from "./pages/ProductPage";
 import CartPage from "./pages/CartPage";
 import MaterialsPage from "./pages/MaterialsPage";
 import InspirationPage from "./pages/InspirationPage";
+import AuthPage from "./pages/AuthPage";
+import ProfilePage from "./pages/ProfilePage"; // Added ProfilePage
 import ScrollToTop from "./components/ScrollToTop";
 import "./assets/styles/global.css";
 import AOS from "aos";
@@ -26,16 +28,18 @@ const App = () => {
   return (
     <>
       <Navbar />
-       {/* Ensures scrolling to top on every route */}
-      {/* <ScrollToTop /> */}
       <div className="content-wrapper">
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/clothing" element={<ClothingPage />} />
-          <Route path="/product/:productId" element={[<ScrollToTop />,<ProductPage />]} />
+          <Route path="/product/:productId" element={[<ScrollToTop />, <ProductPage />]} />
           <Route path="/materials" element={[<ScrollToTop />, <MaterialsPage />]} />
-          <Route path="/inspiration" element={[<ScrollToTop />,<InspirationPage />]} />
+          <Route path="/inspiration" element={[<ScrollToTop />, <InspirationPage />]} />
           <Route path="/cart" element={<CartPage />} />
+          <Route path="/login" element={<AuthPage type="login" />} />
+<Route path="/signup" element={<AuthPage type="signup" />} />
+
+          <Route path="/profile" element={<ProfilePage />} /> {/* Added Route */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </div>
---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,27 @@
+/* eslint-disable react/prop-types */
+import React, { createContext, useContext, useState } from "react";
+
+const AuthContext = createContext();
+
+export const useAuth = () => useContext(AuthContext);
+
+export const AuthProvider = ({ children }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [user, setUser] = useState(null);
+
+  const login = (userData) => {
+    setIsLoggedIn(true);
+    setUser(userData);
+  };
+
+  const logout = () => {
+    setIsLoggedIn(false);
+    setUser(null);
+  };
+
+  return (
+    <AuthContext.Provider value={{ isLoggedIn, user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
